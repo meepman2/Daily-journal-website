c771b98cab846b991bc04680353355294e035d8d
varun sachdeva <sachdevavarun7@gmai.com>
2020-11-04 04:35:49 +0530

    add Procfile

--- app.js
+++ app.js
@@ -76,6 +76,10 @@ const requestedPostId = req.params.postId;
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("started on port 3000");
 });
